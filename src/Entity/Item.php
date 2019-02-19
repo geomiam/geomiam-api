9444c678fb3d36e9c14fc961c179094a8905f996
cvilleger <villeger.c@gmail.com>
2019-02-19 02:38:04 +0100

    Add Maker and Item entity and repositoy

---
+++ src/Entity/Item.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ItemRepository")
+ */
+final class Item
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column()
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    private $pictureUrl;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    private $googleMapsUrl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPictureUrl(): ?string
+    {
+        return $this->pictureUrl;
+    }
+
+    public function setPictureUrl(?string $pictureUrl): self
+    {
+        $this->pictureUrl = $pictureUrl;
+
+        return $this;
+    }
+
+    public function getGoogleMapsUrl(): ?string
+    {
+        return $this->googleMapsUrl;
+    }
+
+    public function setGoogleMapsUrl(?string $googleMapsUrl): self
+    {
+        $this->googleMapsUrl = $googleMapsUrl;
+
+        return $this;
+    }
+}
