ef7d6e6291c843aecd5306bfb5c86ced2d8b1fd0
cvilleger <villeger.c@gmail.com>
2019-02-22 02:38:44 +0100

    Add Homepage

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -5,9 +5,19 @@ namespace App\Controller;
 use App\Repository\ItemRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractFOSRestController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('<h1>42</h1>');
+    }
+
     /**
      * @Rest\Get("/api/items")
      */
